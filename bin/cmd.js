66363ec2ef6976adef87c998de582343d739ccae
James Halliday <mail@substack.net>
2014-10-10 20:14:08 +0800

    functional command

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,7 +2,7 @@
 
 var parse = require('../');
 var fs = require('fs');
-var minimsit = require('minimist');
+var minimist = require('minimist');
 var gunzip = require('zlib').createGunzip;
 
 var argv = minimist(process.argv.slice(2));
@@ -10,15 +10,15 @@ if (argv.help || argv.h) {
     console.log('usage: parse-dictd -i IFILE -d DFILE');
     return process.exit(0);
 }
-if (!argv.d || !argv.i) {
+var dfile = argv.d || argv._.shift();
+var ifile = argv.i || argv._.shift();
+if (!dfile || !ifile) {
     console.error('usage: parse-dictd -i IFILE -d DFILE');
     return process.exit(1);
 }
 
-var dstream = fs.createReadStream(argv.d || arg._.shift())
-    .pipe(gunzip())
-;
-var istream = fs.createReadStream(argv.i || argv._.shift();
+var dstream = fs.createReadStream(dfile).pipe(gunzip())
+var istream = fs.createReadStream(ifile);
 
 parse(dstream, istream).on('data', console.log);
 process.stdout.on('error', process.exit);
